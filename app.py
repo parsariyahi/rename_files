a8dbc4dda63086045effe9bc3425d2dbca2d7a42
parsariyahi <parsanintendo12@gmail.com>
2021-11-21 22:52:12 +0330

    check ext - debug

--- app.py
+++ app.py
@@ -9,20 +9,28 @@ def get_file_name (path : str):
         if dir[2] :
             if not dir[0].startswith('./.') :
                 yield dir[0], dir[2]
+                
 
 def rename_file (files : list, file_path : str) :
     for file in files :
         # split the extention
         file_split = os.path.splitext(file)
-        # split month and day
-        re_file  = file_split[0].split('-')
-        # put new file name and the extension
-        re_file = re_file[1] + '-' + re_file[0] + file_split[1]
-        # create full path of new and old file name
-        full_file_path = f"{file_path}/{file}"
-        full_re_file_path = f"{file_path}/{re_file}"
-        # rename file with its path
-        os.rename(full_file_path, full_re_file_path)
+        
+        #check the extension of the file so we dont edit the pdf or something else
+        if file_split[1] in ['.mp4', '.wmv'] :    
+            # split month and day
+            re_file  = file_split[0].split('-')
+            # put new file name and the extension
+            re_file = re_file[1] + '-' + re_file[0] + file_split[1]
+            # create full path of new and old file name
+            full_file_path = f"{file_path}/{file}"
+            full_re_file_path = f"{file_path}/{re_file}"
+            # rename file with its path
+            os.rename(full_file_path, full_re_file_path)
+
+
+
+
 
 
 for file_path, file_name in get_file_name('./classes/') :
