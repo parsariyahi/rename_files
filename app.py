bc0b02d99329aa7d6c23cc384808161157812499
parsariyahi <parsanintendo12@gmail.com>
2021-11-22 09:22:05 +0330

    add regex for check the patterns file - handel file exist error

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import os
+import re
+import string
+import random
+
+
 
 # get all file name are in the current dir
 def get_file_name (path : str):
@@ -13,25 +18,47 @@ def get_file_name (path : str):
 
 def rename_file (files : list, file_path : str) :
     for file in files :
-        # split the extention
-        file_split = os.path.splitext(file)
+        # split file name and extention 
+        file_name, file_ext = os.path.splitext(file)
         
-        #check the extension of the file so we dont edit the pdf or something else
-        if file_split[1] in ['.mp4', '.wmv'] :    
-            # split month and day
-            re_file  = file_split[0].split('-')
-            # put new file name and the extension
-            re_file = re_file[1] + '-' + re_file[0] + file_split[1]
-            # create full path of new and old file name
-            full_file_path = f"{file_path}/{file}"
-            full_re_file_path = f"{file_path}/{re_file}"
-            # rename file with its path
-            os.rename(full_file_path, full_re_file_path)
+        if re.match(r"(^[0-9]{1,2})-([0-9]{1,2}$)", file_name) :
+                
+            #check the extension of the file
+            if file_ext in ['.mp4', '.wmv'] :
+                
+                # split our format
+                rename  = file_name.split('-')
+                
+                
+                try :
+                    # put new file name and extension together
+                    new_file = f"{rename[1]}-{rename[0]}{file_ext}"
+                    
+                    # create full path for new and old file name
+                    old_file_path = f"{file_path}/{file}"
+                    new_file_path = f"{file_path}/{new_file}"
+                    
+                    # rename file with its path
+                    os.rename(old_file_path, new_file_path)
+
+                #if file exist this will handel it
+                except FileExistsError :
+                    #all letters
+                    letters = string.ascii_lowercase
+                    #if file exist this will create a random string
+                    rand_str = ''.join(random.choice(letters) for _ in range(3))
+                    new_file = f"{rename[1]}-{rename[0]}_{rand_str}{file_ext}"
+                    
+                    # create full path for new and old file name
+                    old_file_path = f"{file_path}/{file}"
+                    new_file_path = f"{file_path}/{new_file}"
+                    
+                    # rename file with its path
+                    os.rename(old_file_path, new_file_path)
+                    print(f"{new_file_path=} ---- {old_file_path=}")
 
 
+if __name__ == "__main__" :    
 
-
-
-
-for file_path, file_name in get_file_name('./classes/') :
-    rename_file(file_name, file_path)
+    for file_path, file_name in get_file_name('./classes/') :
+        rename_file(file_name, file_path)
